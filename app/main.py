41a9c01cb3b17ca00f496c7aca463028adab2b68
putupradnya <putuandhika@hotmail.com>
2023-12-13 13:29:00 +0700

    add dynamic table

--- app/main.py
+++ app/main.py
@@ -3,19 +3,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.graph_objects as go
 
-# Function to create a placeholder plot
-def create_placeholder_plot():
-    fig, ax = plt.subplots()
-    ax.plot([0, 1], [0, 1], 'o')
-    ax.annotate('A/N', xy=(0.5, 0.5), xytext=(0.5, 0.5), textcoords='axes fraction')
-    plt.close(fig)
-    return fig
 
-def load_txt(file):
-    # Assuming the .txt file is a CSV without a header
-    # You may need to adjust this depending on the structure of your .txt file
-    return pd.read_csv(file, header=None)
+def runInversion(initModel):
+    initModel.to_excel('output.xlsx')
+
     
 # Set up the Streamlit layout according to the sketch
 def main():
@@ -55,25 +48,34 @@ def main():
                 dumping = st.number_input('Damping Factor')
             
 
-        model_data = pd.DataFrame({
-        'Model Number': range(1, 6),
-        'Thickness': np.random.rand(5) * 10  # Random thickness for demonstration
-        })
-        st.data_editor(model_data)
+        modelData = pd.DataFrame(columns = ['Thickness', 'Rho Apparent'])
+        configs = {
+                    'Thickness' : st.column_config.NumberColumn('Thickness'), 
+                    'Rho Apparent' : st.column_config.NumberColumn('Rho Apparent', required=True, min_value=0, max_value=1000) 
+                    }
+        initModel = st.data_editor(modelData, hide_index=True, column_config=configs, num_rows='dynamic')
+        
+        df =  pd.DataFrame(columns = ['Thickness', 'Rho Apparent'])
+        edited_df = st.data_editor(
+            df,
+            use_container_width=True,
+            num_rows="dynamic",
+        )
+        if (uploadFile is None) and (optionInversion == '-'):
+            st.button('RUN INVERSION', disabled=True)
+        else:
+            st.button('RUN INVERSION', on_click = runInversion(initModel))
+
+
     with col3:
         containerPlot = st.container(border=True)
 
         with containerPlot:
 
-            tab1, tab2 = st.tabs(['Inversion Model', 'Earth Model'])
+            tab1, tab2, tab3 = st.tabs(['Resistivity Curve', 'RMS Error', 'Earth Model'])
             
             with tab1:
                 if uploadFile is not None:
-                    # fig, ax = plt.subplots()
-                    # ax.plot(df['AB/2'], df['RhoApp'])
-                    # ax.set_xlabel(f'AB/2')
-                    # ax.set_ylabel(f'RhoApp')
-                    # st.pyplot(fig)
                     fig = px.scatter(df, x='AB/2', y='RhoApp', title='Resistivity Curve')
                     fig.update_layout(
                         xaxis_type="log",
@@ -84,6 +86,40 @@ def main():
                     
                     # Display the figure in Streamlit
                     st.plotly_chart(fig, use_container_width=True)
+
+            with tab2:
+                # Generate some sample data
+                x2 = np.arange(100)
+                y2 = -np.log(x2)
+
+                # Create a step plot using Plotly
+                fig2 = go.Figure(data=go.Scatter(x=x2, y=y2, mode='lines+markers', line_shape='hv'))
+
+                # Set plot layout
+                fig2.update_layout(
+                    title='Iteration Vs RMSE',
+                    xaxis_title='N Iter',
+                    yaxis_title='RMSE'
+                )
+                st.plotly_chart(fig2)
+
+            with tab3:
+                # Generate some sample data
+                x3 = np.linspace(0, 10, 5)
+                y3 = np.random.uniform(5, 100, len(x3))
+
+                # Create a step plot using Plotly
+                fig3 = go.Figure(data=go.Scatter(x=y3, y=x3, mode='lines+markers', line_shape='vh'))
+
+                # Set plot layout
+                fig3.update_layout(
+                    title='Resistivity Model',
+                    xaxis_title=f'Resistivity [Ω.m]',
+                    yaxis_title='Depth [m]',
+                    yaxis=dict(autorange="reversed")
+                )
+                st.text('MODEL')
+                st.plotly_chart(fig3)
 
 if __name__ == "__main__":
     main()
